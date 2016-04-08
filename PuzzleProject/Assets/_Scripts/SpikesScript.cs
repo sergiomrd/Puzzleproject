c29cb07a375c2eb4aeb3b1765fc8ff0c97b8ee2f
Sergio Malonda <sergio.del93@gmail.com>
2016-04-08 23:54:20 +0200

    Prueba 1

---
+++ PuzzleProject/Assets/_Scripts/SpikesScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikesScript : MonoBehaviour {
+
+	public bool IsActivable;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
